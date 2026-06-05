2b2b355266d7bdf94f8e1b3846e1a071c85a7561
unknown <your-email@example.com>
2026-06-05 14:38:04 +0300

    Fix middleware matcher for next16

--- middleware.ts
+++ middleware.ts
@@ -3,6 +3,5 @@ import { clerkMiddleware } from "@clerk/nextjs/server";
 export default clerkMiddleware();
 
 export const config = {
-  // كود مبسط متوافق تماماً مع محرك Next.js 16 بدون رموز معقدة تسبب انهيار الـ Build
   matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
 };
